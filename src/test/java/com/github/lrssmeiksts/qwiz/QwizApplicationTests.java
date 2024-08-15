188a2fdfea2d7ec19a9bf9c3f61d34c2a85b8713
LRSSmeiksts <lauris.smeiksts@gmail.com>
2024-08-15 14:45:00 +0300

    first commit

---
+++ src/test/java/com/github/lrssmeiksts/qwiz/QwizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.lrssmeiksts.qwiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QwizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
